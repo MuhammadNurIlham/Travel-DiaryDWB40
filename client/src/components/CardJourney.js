7a12f578a184a246c098b06cd2b238d80599c56f
Muhammad Nur Ilham <muhnilham99@gmail.com>
2022-11-12 03:57:20 +0700

    fetching data from database backend

--- client/src/components/CardJourney.js
+++ client/src/components/CardJourney.js
@@ -1,26 +1,36 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
+import { API } from '../config/API';
 import image1 from '../assets/image1.png'
-import image2 from '../assets/image2.png'
-import image3 from '../assets/image3.png'
-import image4 from '../assets/image4.png'
 
 function CardJourney() {
+    let { data: journeys } = useQuery("journeysCache", async () => {
+        const response = await API.get("/journeys");
+        return response.data.data
+    })
+
+
     return (
         <div className='container'>
-            <div className="row row-cols-1 row-cols-md-4 g-4">
-                <div className="col pt-4">
-                    <div className="card h-100">
-                        <img src={image1} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <Link to="/DetailJourney">
-                                <h5 className="card-title">Card title</h5>
-                            </Link>
-                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            {journeys?.length !== 0 ? (
+                <div className="row row-cols-1 row-cols-md-4 g-4">
+                    {journeys?.map((item) => (
+                        <div className="col pt-4">
+                            <div className="card h-100">
+                                <img src={item?.image} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <Link to="/DetailJourney">
+                                        <h5 className="card-title">{item?.title}</h5>
+                                    </Link>
+                                    <p className="card-text">{item?.description}</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    )
+                    )}
                 </div>
-            </div>
+            ) : (<div>Halo halo haloooooooooo</div>)}
         </div>
     )
 }
