7161a71fe42e462bdcebac876e6c56da46dc007f
Muhammad Nur Ilham <muhnilham99@gmail.com>
2022-11-12 02:22:52 +0700

    insert data

--- client/src/components/Editor.js
+++ client/src/components/Editor.js
@@ -1,9 +1,70 @@
 import React, { useState, useEffect } from 'react';
 import { Form } from 'react-bootstrap';
+import { useMutation } from 'react-query';
+import { useNavigate, useParams } from 'react-router-dom';
 import NavbarAfterLogin from './NavbarAfterLogin';
+import { API } from '../config/API';
 
 function Editor() {
+    let navigate = useNavigate();
+    const [journeys, setJourneys] = useState([]);
     const [preview, setPreview] = useState(null); // set for image preview
+    const [form, setForm] = useState({
+        image: "",
+        title: "",
+        description: "",
+    });
+
+    const getJourneys = async () => {
+        try {
+            const response = await API.get('/journeys');
+            setJourneys(response.data.data);
+        } catch (error) {
+            console.log('ini error get journey', error)
+        }
+    };
+
+    const handleOnChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]:
+                e.target.type === "file" ? e.target.files : e.target.value,
+        });
+
+        //create image preview
+        if (e.target.type === "file") {
+            let url = URL.createObjectURL(e.target.files[0]);
+            setPreview(url);
+        }
+    };
+
+    // const {id} = useParams();
+    const handleOnSubmit = useMutation(async (e) => {
+        try {
+            e.preventDefault();
+
+            const formData = new FormData();
+            formData.set("image", form.image[0], form.image[0].name);
+            formData.set("title", form.title);
+            formData.set("description", form.description);
+
+            const response = await API.post('/journey', formData, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.token}`
+                },
+            });
+            navigate('/Home')
+            // navigate(`journey/${id}`)
+            console.log("ini response add journey", response);
+        } catch (error) {
+            console.log("ini response error add journey", error);
+        }
+    });
+
+    useEffect(() => {
+        getJourneys();
+    }, []);
+
 
     return (
         <div>
@@ -12,7 +73,7 @@ function Editor() {
                 <div>
                     <h3 className='title py-3 text-dark'>Add Journey</h3>
                 </div>
-                <Form>
+                <Form onSubmit={(e) => handleOnSubmit.mutate(e)}>
                     {preview && (
                         <div>
                             <img
@@ -27,16 +88,15 @@ function Editor() {
                             />
                         </div>
                     )}
-
                     <div className='row'>
                         <div className='col mb-3'>
-                            <input type="text" className="form-control px-2 py-3" id="exampleFormControlInput1" placeholder="Title" name='name'></input>
+                            <input type="text" className="form-control px-2 py-3" id="exampleFormControlInput1" placeholder="Title" name='title' onChange={handleOnChange}></input>
                         </div>
                     </div>
                     <div className='row'>
                         <div className='col'>
                             <div className="input-group mb-3">
-                                <input type="file" className="form-control" id="inputGroupFile02" />
+                                <input type="file" name='image' className="form-control" id="inputGroupFile02" onChange={handleOnChange} />
                                 <label className="input-group-text" for="inputGroupFile02">Upload</label>
                             </div>
                         </div>
@@ -48,7 +108,8 @@ function Editor() {
                                 className="form-control px-2 py-3"
                                 id="exampleFormControlInput1"
                                 placeholder="Description"
-                                name='desc'
+                                name='description'
+                                onChange={handleOnChange}
                                 style={{
                                     height: "200px"
                                 }}></textarea>
