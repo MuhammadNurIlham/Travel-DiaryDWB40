584be5172c6bda7d0aea87d60c409044dbf3871e
Muhammad Nur Ilham <muhnilham99@gmail.com>
2022-11-13 15:30:36 +0700

    update handle bookmark

--- client/src/components/CardJourney.js
+++ client/src/components/CardJourney.js
@@ -1,43 +1,111 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { useQuery } from 'react-query';
 import { Link, useNavigate } from 'react-router-dom';
 import { API } from '../config/API';
+import Swal from 'sweetalert2';
 import { FaBookmark } from "react-icons/fa"
 import { FaRegBookmark } from "react-icons/fa"
 import { FaHeart } from "react-icons/fa"
 import { FaRegHeart } from "react-icons/fa"
 
 import image1 from '../assets/image1.png'
+import { UserContext } from '../context/UserContext';
 
 function CardJourney() {
     let navigate = useNavigate();
 
+    const [state] = useContext(UserContext);
+    const isLogin = state.isLogin;
+    console.log("state buat bookmark euy", state);
+
     let { data: journeys } = useQuery("journeysCache", async () => {
         const response = await API.get("/journeys");
+        console.log("ini response journeys", response)
+        const resultResponse = response.data.data;
+        console.log("ini result response", resultResponse)
         return response.data.data
     })
+
+    const handleOnBookmark = async (e, journeyID) => {
+        e.preventDefault();
+        try {
+            console.log("mau bookmark yang ini :", state.user.id)
+
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${localStorage.token}`,
+                },
+            };
+            console.log("data check journey", journeyID, state.user.id);
+
+            const response = await API.post("/bookmark", {
+                journey_id: parseInt(journeyID),
+                user_id: parseInt(state.user.id),
+            }, config);
+            console.log("response post bookmark euy", response)
+        } catch (error) {
+            console.log("ini error di post bookmark", error)
+        }
+    }
 
     return (
         <div className='container'>
             {journeys?.length !== 0 ? (
                 <div className="row row-cols-1 row-cols-md-4 g-4">
-                    {journeys?.map((item, index) => (
-                        <div className="col pt-4">
+                    {journeys?.map((journey, index) => (
+                        <div className="col pt-4" key={index}>
                             <div className="card h-100">
-                                <img src={item?.image} className="card-img-top" alt="..." />
+                                <img src={journey?.image} className="card-img-top" alt="..." />
                                 <div className="card-body">
-                                    <div className='d-flex pb-3'>
-                                        <span className='pe-1'><FaRegHeart /></span>
-                                        <Link to="/Bookmark">
-                                            <span className='pe-1'><FaRegBookmark /></span>
-                                        </Link>
-                                        {/* <span className='pe-1'><FaHeart /></span>
+                                    {isLogin ? (
+                                        <div className='d-flex pb-3'>
+                                            <span className='pe-1'><FaRegHeart /></span>
+                                            <span className='pe-1' onClick={(e) => {
+                                                Swal.fire({
+                                                    title: 'Do you want to save this journey?',
+                                                    showDenyButton: true,
+                                                    showCancelButton: true,
+                                                    confirmButtonText: 'Save',
+                                                    denyButtonText: `Don't save`,
+                                                }).then((result) => {
+                                                    /* Read more about isConfirmed, isDenied below */
+                                                    if (result.isConfirmed) {
+                                                        Swal.fire({
+                                                            icon: "success",
+                                                            title: "Success!",
+                                                            showConfirmButton: true,
+                                                            onClick: handleOnBookmark(e, journey.id),
+                                                        });
+                                                    } else if (result.isDenied) {
+                                                        Swal.fire('Journey are not saved', '', 'info')
+                                                    }
+                                                })
+                                            }}><FaRegBookmark /></span>
+                                            {/* <Link to="/Bookmark">
+                                        </Link> */}
+                                            {/* <span className='pe-1'><FaHeart /></span>
                                         <span><FaBookmark /></span> */}
-                                    </div>
-                                    <h5 className="card-title" onClick={() => { navigate(`/DetailJourney/${item?.id}`) }} key={index}>{item?.title}</h5>
+                                        </div>
+                                    ) : (
+                                        <div className='d-flex pb-3'>
+                                            <span className='pe-1'><FaRegHeart /></span>
+                                            <span className='pe-1' onClick={(e) => {
+                                                Swal.fire({
+                                                    icon: 'error',
+                                                    title: 'Oops...',
+                                                    text: 'Anda Belum Login, Silahkan Login!',
+                                                })
+                                            }}><FaRegBookmark /></span>
+                                            {/* <Link to="/Bookmark">
+                                        </Link> */}
+                                            {/* <span className='pe-1'><FaHeart /></span>
+                                        <span><FaBookmark /></span> */}
+                                        </div>
+                                    )}
+                                    <h5 className="card-title" onClick={() => { navigate(`/DetailJourney/${journey?.id}`) }} key={index}>{journey?.title}</h5>
                                     {/* <div className='d-flex'>
                                     </div> */}
-                                    <p className="card-text">{item?.description}</p>
+                                    <p className="card-text">{journey?.description}</p>
                                 </div>
                             </div>
                         </div>
--- client/src/pages/Bookmark.js
+++ client/src/pages/Bookmark.js
@@ -1,14 +1,141 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useQuery } from 'react-query';
+import { useNavigate } from 'react-router-dom';
 import CardJourney from '../components/CardJourney'
 import NavbarAfterLogin from '../components/NavbarAfterLogin';
+import { UserContext } from '../context/UserContext';
+import Swal from 'sweetalert2';
+import { API } from "../config/API";
+import { FaBookmark } from "react-icons/fa"
+import { FaRegBookmark } from "react-icons/fa"
+import { FaHeart } from "react-icons/fa"
+import { FaRegHeart } from "react-icons/fa"
 
 function Bookmark() {
+    const [state] = useContext(UserContext);
+    console.log("ini state profile bookmark yak ges yak", state.user)
+    const isLogin = state.isLogin;
+
+    let navigate = useNavigate();
+    let { data: bookmarks, refetch: yukRefetch } = useQuery(
+        "bookmarksCache", async () => {
+            const response = await API.get("/bookmarks");
+            console.log("ini response get bookmark ygy", response)
+            const resultResponse = response.data.data
+            console.log("ini resultResponse uhuyy", bookmarks)
+
+            const resultFilter = resultResponse.filter(
+                (data) => data.user.id === state.user.id
+            );
+            console.log("resulFilter weyy", resultFilter);
+            return resultFilter
+        }
+    );
+    console.log("ini isi bookmark : ", bookmarks)
+
+    const handleDelete = async (e, bookmarkId) => {
+        e.preventDefault();
+        try {
+            console.log("mau delete yg ini ygy", state.user.id);
+
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${localStorage.token}`,
+                },
+            };
+            console.log("mau cek data", bookmarkId, state.user.id);
+
+            const response = await API.delete(`/bookmark/${bookmarkId}`, config);
+            console.log("ini response delete ygy", response);
+            yukRefetch();
+            navigate('/Bookmark')
+        } catch (error) {
+            console.log("waduh deletenya error", error);
+        }
+    };
+
+    useEffect(() => {
+        if (!localStorage.getItem("token")) {
+            Swal.fire({
+                icon: "error",
+                title: "Heyyoo!",
+                text: "You have to login first~",
+            });
+            navigate("/")
+        }
+    }, []);
+
+
+
     return (
         <div>
             <NavbarAfterLogin />
             <div className='container'>
                 <h2 className='text-start py-4 heading-journey'>Bookmark</h2>
-                <CardJourney />
+                <div className="row row-cols-1 row-cols-md-4 g-4">
+                    {bookmarks?.map((bookmark, index) => {
+                        return (
+                            <div className="col pt-4" key={index}>
+                                <div className="card h-100">
+                                    <img src={bookmark.journey.image} className="card-img-top" alt="..." />
+                                    <div className="card-body">
+                                        {isLogin ? (
+                                            <div className='d-flex pb-3 ms-auto'>
+                                                <span className='pe-1'><FaRegHeart /></span>
+                                                <span className='pe-1' onClick={(e) => {
+                                                    Swal.fire({
+                                                        title: 'Do you want to delete this journey from bookmark?',
+                                                        showDenyButton: true,
+                                                        showCancelButton: true,
+                                                        confirmButtonText: 'Delete',
+                                                        denyButtonText: `Cancel`,
+                                                    }).then((result) => {
+                                                        /* Read more about isConfirmed, isDenied below */
+                                                        if (result.isConfirmed) {
+                                                            Swal.fire({
+                                                                icon: "success",
+                                                                title: "Delete Success!",
+                                                                showConfirmButton: true,
+                                                                onClick: handleDelete(e, Bookmark.ID),
+                                                            });
+                                                        } else if (result.isDenied) {
+                                                            Swal.fire('Journey are not delete', '', 'info')
+                                                        }
+                                                    })
+                                                }}><FaRegBookmark /></span>
+                                                {/* <Link to="/Bookmark">
+                                        </Link> */}
+                                                {/* <span className='pe-1'><FaHeart /></span>
+                                        <span><FaBookmark /></span> */}
+                                            </div>
+                                        ) : (
+                                            <div className='d-flex pb-3'>
+                                                <span className='pe-1'><FaRegHeart /></span>
+                                                <span className='pe-1' onClick={(e) => {
+                                                    Swal.fire({
+                                                        icon: 'error',
+                                                        title: 'Oops...',
+                                                        text: 'Anda Belum Login, Silahkan Login!',
+                                                    })
+                                                }}><FaRegBookmark /></span>
+                                                {/* <Link to="/Bookmark">
+                                        </Link> */}
+                                                {/* <span className='pe-1'><FaHeart /></span>
+                                        <span><FaBookmark /></span> */}
+                                            </div>
+                                        )}
+                                        <h5 className="card-title" onClick={() => { navigate(`/DetailJourney/${bookmark.journey.id}`) }} key={index}>{bookmark.journey.title.slice(0, 20)} ..</h5>
+                                        <p className="card-text">{bookmark.journey.user.name}</p>
+                                        {/* <div className='d-flex'>
+                                    </div> */}
+                                        <p className="card-text text-justify">{bookmark.journey.description.slice(0, 120)} ...</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
+                    )}
+                </div>
             </div>
         </div>
     )
